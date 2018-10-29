12d39599400a7d3a17424a1fb455363b60f7cbb0
Gergo Hajdu <gohajdu@gmail.com>
2018-10-29 00:02:13 -0400

    Theme overhaul

--- app/src/main/java/bigchicken/secretive/CaesarCipher.java
+++ app/src/main/java/bigchicken/secretive/CaesarCipher.java
@@ -1,18 +1,22 @@
 package bigchicken.secretive;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class CaesarCipher extends AppCompatActivity {
 
     private String plainText;
     private String cipherText;
-    private int inputKey;
+    private int inputKey = 0;
     private boolean invalidKey;
 
     @Override
@@ -27,8 +31,7 @@ public class CaesarCipher extends AppCompatActivity {
         if (invalidKey) return; // If entered key value is invalid, the method stops
         cipherText = encrypt(plainText, inputKey);
 
-        // Changing layout to show output text
-        setContentView(R.layout.output_layout);
+        startOutput();
     }
 
     // Action when user clicks Decryption button
@@ -38,8 +41,7 @@ public class CaesarCipher extends AppCompatActivity {
         inputKey *= -1; // inputKey is negated for decryption so same method can be used
         cipherText = encrypt(plainText, inputKey);
 
-        // Changing layout to show output text
-        setContentView(R.layout.output_layout);
+        startOutput();
     }
 
     // Get all text entered by user
@@ -92,5 +94,27 @@ public class CaesarCipher extends AppCompatActivity {
 
         // The finished charText is converted into a String and returned
         return new String(charText);
+    }
+
+    // Changes layout to output_layer
+    // Sets up output layout text and copy button
+    private void startOutput(){
+        // Changing layout to show output text
+        setContentView(R.layout.output_layout);
+
+        // TextView in output_layout is set to the final encrypted/decrypted text
+        TextView outputView = (TextView) findViewById(R.id.finalOutput);
+        outputView.setText(cipherText);
+
+        // Setting up Copy button to copy to clipboard
+        Button copyButton = (Button) findViewById(R.id.copyButton);
+        copyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ClipboardManager clipboardManager = (ClipboardManager)getSystemService(Context.CLIPBOARD_SERVICE);
+                clipboardManager.setPrimaryClip(ClipData.newPlainText("outputText", cipherText));
+                Toast.makeText(CaesarCipher.this, "Copied to clipboard", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
